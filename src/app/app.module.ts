9ca69b0a947a596517678f23f0bf4aebb5a8d2b8
han254 <peternjenga1706@gmail.com>
2022-04-09 05:36:30 +0300

    create myquotes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MyquotesComponent } from './myquotes/myquotes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MyquotesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/myquotes/myquotes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myquotes',
+  templateUrl: './myquotes.component.html',
+  styleUrls: ['./myquotes.component.css']
+})
+export class MyquotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
